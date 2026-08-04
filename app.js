4e06ab084abd1ec702c4b29d245a2c59a12bfb4f
carc0813 <carc1023@hotmail.com>
2026-08-04 11:56:55 -0500

    algunos cambios

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 import axios from "axios";
 
 const API_KEY = "OYxg6CHgqlStw3QQI9noYXNIfHm9MScpETZp1y5I";
@@ -17,24 +15,21 @@ export async function obtenerAPOD(fecha = "") {
     return respuesta.data;
 }
 
-
 export function mostrarAPOD(datos) {
 
     const contenedor = document.getElementById("apod");
 
     contenedor.innerHTML = `
-    
         <h2>${datos.title}</h2>
 
-        <p>${datos.date}</p>
+        <p><strong>Fecha:</strong> ${datos.date}</p>
 
         ${
             datos.media_type === "image"
-                ? `<img src="${datos.url}" width="600">`
-                : `<iframe src="${datos.url}" width="600" height="400"></iframe>`
+                ? `<img src="${datos.url}" alt="${datos.title}" style="max-width:100%; border-radius:10px;">`
+                : `<iframe src="${datos.url}" width="100%" height="500"></iframe>`
         }
 
         <p>${datos.explanation}</p>
-
     `;
 }
